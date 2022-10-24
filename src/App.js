f74d572c964c10991e36f4a3ccb5943800f1a14d
Adil Yıldız <yildizadil81@gmail.com>
2022-10-24 23:46:23 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import "./App.scss";
+
+import React from "react";
+import Navbar from "./components/navbar/Navbar";
+import Header from "./components/header/Header";
+import Main from "./components/main/Main";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Navbar />
+      <Header />
+      <Main />
+    </div>
+  );
+};
+
+export default App;
